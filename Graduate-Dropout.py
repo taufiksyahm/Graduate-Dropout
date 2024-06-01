38d600ade02b0d3d6a1d7f524e3408b3f86b5cc7
Taufik Syah Mauludin <taufiksyah24@gmail.com>
2024-06-01 13:08:42 +0700

    Update Graduate-Dropout.py

--- Graduate-Dropout.py
+++ Graduate-Dropout.py
@@ -30,6 +30,26 @@ with col1:
 with col2:
     Previous_qualification = st.selectbox(label='Previous_qualification', options=encoder_Previous_qualification.classes_, index=1)
     data["Previous_qualification"] = [Previous_qualification]
+
+col1, col2 = st.columns(2)
+     
+with col1:
+    Fathers_occupation = st.selectbox(label='Fathers_occupation', options=encoder_Fathers_occupation.classes_, index=1)
+    data["Fathers_occupation"] = [Fathers_occupation]
+     
+with col2:
+    Fathers_qualification = st.selectbox(label='Fathers_qualification', options=encoder_Fathers_qualification.classes_, index=1)
+    data["Fathers_qualification"] = [Fathers_qualification]
+
+col1, col2 = st.columns(2)
+     
+with col1:
+    Mothers_occupation = st.selectbox(label='Mothers_occupation', options=encoder_Mothers_occupation.classes_, index=1)
+    data["Mothers_occupation"] = [Mothers_occupation]
+     
+with col2:
+    Mothers_qualification = st.selectbox(label='Mothers_qualification', options=encoder_Mothers_qualification.classes_, index=1)
+    data["Mothers_qualification"] = [Mothers_qualification]
    
 col1, col2 = st.columns(2)
      
