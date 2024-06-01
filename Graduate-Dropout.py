af341f7faf724c0dcc899858314fad2ee30a1053
Taufik Syah Mauludin <taufiksyah24@gmail.com>
2024-06-01 12:04:25 +0700

    Add files via upload

---
+++ Graduate-Dropout.py
@@ -0,0 +1,64 @@
+import streamlit as st
+import pandas as pd
+import joblib
+from data_preprocessing import data_preprocessing, encoder_Application_mode, encoder_Course, encoder_Fathers_occupation, encoder_Fathers_qualification, encoder_Marital_status, encoder_Mothers_occupation, encoder_Mothers_qualification, encoder_Nacionality, encoder_Previous_qualification
+from prediction import prediction
+
+col1 = st.columns([1, 5])
+with col1:
+    st.header('Graduate-Dropout App (Prototype)')
+
+    data = pd.DataFrame()
+     
+col1, col2, col3 = st.columns(3)
+     
+with col1:
+    Application_mode = st.selectbox(label='Application_mode', options=encoder_Application_mode.classes_, index=1)
+    data["Application_mode"] = [Application_mode]
+     
+with col2:
+    Course = st.selectbox(label='Course', options=encoder_Course.classes_, index=1)
+    data["Course"] = [Course]
+     
+with col3:
+    Marital_status = st.selectbox(label='Marital_status', options=encoder_Marital_status.classes_, index=5)
+    data["Marital_status"] = Marital_status
+
+col1, col2 = st.columns(2)
+     
+with col1:
+    Nacionality = st.selectbox(label='Nationality', options=encoder_Nacionality.classes_, index=1)
+    data["Nacionality"] = [Nacionality]
+     
+with col2:
+    Previous_qualification = st.selectbox(label='Previous_qualification', options=encoder_Previous_qualification.classes_, index=1)
+    data["Previous_qualification"] = [Previous_qualification]
+   
+col1, col2 = st.columns(2)
+     
+with col1:
+    Curricular_units_1st_sem_approved = int(st.number_input(label='Curricular_units_1st_sem_approved', value=0))
+    data["Curricular_units_1st_sem_approved"] = Curricular_units_1st_sem_approved
+     
+with col2:
+    Curricular_units_1st_sem_grade = int(st.number_input(label='Curricular_units_1st_sem_grade', value=0))
+    data["Curricular_units_1st_sem_grade"] = Curricular_units_1st_sem_grade
+
+col1, col2 = st.columns(2)
+     
+with col1:
+    Curricular_units_2nd_sem_approved = int(st.number_input(label='Curricular_units_2nd_sem_approved', value=0))
+    data["Curricular_units_2nd_sem_approved"] = Curricular_units_2nd_sem_approved
+     
+with col2:
+    Curricular_units_2nd_sem_grade = int(st.number_input(label='Curricular_units_2nd_sem_grade', value=0))
+    data["Curricular_units_2nd_sem_grade"] = Curricular_units_2nd_sem_grade
+     
+with st.expander("View the Raw Data"):
+    st.dataframe(data=data, width=800, height=10)
+
+if st.button('Predict'):
+    new_data = data_preprocessing(data=data)
+    with st.expander("View the Preprocessed Data"):
+        st.dataframe(data=new_data, width=800, height=10)
+    st.write("Status Prediction: {}".format(prediction(new_data)))
---
+++ data_preprocessing.py
@@ -0,0 +1,47 @@
+import joblib
+import numpy as np
+import pandas as pd
+
+encoder_Application_mode = joblib.load("model/encoder_Application_mode.joblib")
+encoder_Course = joblib.load("model/encoder_Course.joblib")
+encoder_Fathers_occupation = joblib.load("model/encoder_Fathers_occupation.joblib")
+encoder_Fathers_qualification = joblib.load("model/encoder_Fathers_qualification.joblib")
+encoder_Marital_status = joblib.load("model/encoder_Marital_status.joblib")
+encoder_Mothers_occupation = joblib.load("model/encoder_Mothers_occupation.joblib")
+encoder_Mothers_qualification = joblib.load("model/encoder_Mothers_qualification.joblib")
+encoder_Nacionality = joblib.load("model/encoder_Nacionality.joblib")
+encoder_Previous_qualification = joblib.load("model/encoder_Previous_qualification.joblib")
+scaler_Curricular_units_1st_sem_approved = joblib.load("model/scaler_Curricular_units_1st_sem_approved.joblib")
+scaler_Curricular_units_1st_sem_grade = joblib.load("model/scaler_Curricular_units_1st_sem_grade.joblib")
+scaler_Curricular_units_2nd_sem_approved = joblib.load("model/scaler_Curricular_units_2nd_sem_approved.joblib")
+scaler_Curricular_units_2nd_sem_grade = joblib.load("model/scaler_Curricular_units_2nd_sem_grade.joblib")
+    
+def data_preprocessing(data):
+    """Preprocessing data
+     
+    Args:
+        data (Pandas DataFrame): Dataframe that contain all the data to make prediction 
+            
+    return:
+        Pandas DataFrame: Dataframe that contain all the preprocessed data
+    """
+    
+    data = data.copy()
+    df = pd.DataFrame()
+    
+    df["Curricular_units_1st_sem_approved"] = scaler_Curricular_units_1st_sem_approved.transform(np.asarray(data["Curricular_units_1st_sem_approved"]).reshape(-1,1))[0]
+    df["Curricular_units_1st_sem_grade"] = scaler_Curricular_units_1st_sem_grade.transform(np.asarray(data["Curricular_units_1st_sem_grade"]).reshape(-1,1))[0]
+    df["Curricular_units_2nd_sem_approved"] = scaler_Curricular_units_2nd_sem_approved.transform(np.asarray(data["Curricular_units_2nd_sem_approved"]).reshape(-1,1))[0]
+    df["Curricular_units_2nd_sem_grade"] = scaler_Curricular_units_2nd_sem_grade.transform(np.asarray(data["Curricular_units_2nd_sem_grade"]).reshape(-1,1))[0]
+        
+    df["Application_mode"] = encoder_Application_mode.transform(data["Application_mode"])[0]
+    df["Course"] = encoder_Course.transform(data["Course"])[0]
+    df["Fathers_occupation"] = encoder_Fathers_occupation.transform(data["Fathers_occupation"])[0]
+    df["Fathers_qualification"] = encoder_Fathers_qualification.transform(data["Fathers_qualification"])[0]
+    df["Marital_status"] = encoder_Marital_status.transform(data["Marital_status"])[0]
+    df["Mothers_occupation"] = encoder_Mothers_occupation.transform(data["Mothers_occupation"])[0]
+    df["Mothers_qualification"] = encoder_Mothers_qualification.transform(data["Mothers_qualification"])[0]
+    df["Nacionality"] = encoder_Nacionality.transform(data["Nacionality"])
+    df["Previous_qualification"] = encoder_Previous_qualification.transform(data["Previous_qualification"])
+        
+    return df
