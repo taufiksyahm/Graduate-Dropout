3470546cf22b8af434df0ad94907de5f77238a73
Taufik Syah Mauludin <taufiksyah24@gmail.com>
2024-06-03 18:11:15 +0700

    Update Graduate-Dropout.py

--- Graduate-Dropout.py
+++ Graduate-Dropout.py
@@ -6,7 +6,6 @@ from prediction import prediction
 
 st.header('Graduate-Dropout App (Prototype)')
 data = pd.DataFrame()
-model = joblib.load("model/rdf_model.joblib")
      
 col1, col2, col3 = st.columns(3)
      
@@ -76,9 +75,7 @@ with st.expander("View the Raw Data"):
     st.dataframe(data=data, width=800, height=10)
 
 if st.button('Predict'):
-    st.write((data.columns))
-    st.write((model.feature_names_in_))
-    #new_data = data_preprocessing(data=data)
-    #with st.expander("View the Preprocessed Data"):
-    #    st.dataframe(data=new_data, width=800, height=10)
-    #st.write("Status: {}".format(prediction(new_data)))
+    new_data = data_preprocessing(data=data)
+    with st.expander("View the Preprocessed Data"):
+        st.dataframe(data=new_data, width=800, height=10)
+    st.write("Status: {}".format(prediction(new_data)))
