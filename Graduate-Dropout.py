ba38e49c0e251601897f3f6f59223504b832245a
Taufik Syah Mauludin <taufiksyah24@gmail.com>
2024-06-03 18:19:51 +0700

    Update Graduate-Dropout.py

--- Graduate-Dropout.py
+++ Graduate-Dropout.py
@@ -10,33 +10,33 @@ data = pd.DataFrame()
 col1, col2, col3 = st.columns(3)
      
 with col1:
+    Marital_status = st.selectbox(label='Marital_status', options=encoder_Marital_status.classes_, index=1)
+    data["Marital_status"] = Marital_status
+     
+with col2:
     Application_mode = st.selectbox(label='Application_mode', options=encoder_Application_mode.classes_, index=1)
     data["Application_mode"] = [Application_mode]
      
-with col2:
+with col3:
     Course = st.selectbox(label='Course', options=encoder_Course.classes_, index=1)
     data["Course"] = [Course]
-     
-with col3:
-    Marital_status = st.selectbox(label='Marital_status', options=encoder_Marital_status.classes_, index=5)
-    data["Marital_status"] = Marital_status
 
 col1, col2 = st.columns(2)
      
 with col1:
-    Nacionality = st.selectbox(label='Nacionality', options=encoder_Nacionality.classes_, index=1)
-    data["Nacionality"] = [Nacionality]
-     
-with col2:
     Previous_qualification = st.selectbox(label='Previous_qualification', options=encoder_Previous_qualification.classes_, index=1)
     data["Previous_qualification"] = [Previous_qualification]
+          
+with col2:
+    Nacionality = st.selectbox(label='Nacionality', options=encoder_Nacionality.classes_, index=1)
+    data["Nacionality"] = [Nacionality]
 
 col1, col2 = st.columns(2)
      
 with col1:
-    Fathers_occupation = st.selectbox(label='Fathers_occupation', options=encoder_Fathers_occupation.classes_, index=1)
-    data["Fathers_occupation"] = [Fathers_occupation]
-     
+    Mothers_qualification = st.selectbox(label='Mothers_qualification', options=encoder_Mothers_qualification.classes_, index=1)
+    data["Mothers_qualification"] = [Mothers_qualification]
+          
 with col2:
     Fathers_qualification = st.selectbox(label='Fathers_qualification', options=encoder_Fathers_qualification.classes_, index=1)
     data["Fathers_qualification"] = [Fathers_qualification]
@@ -48,8 +48,8 @@ with col1:
     data["Mothers_occupation"] = [Mothers_occupation]
      
 with col2:
-    Mothers_qualification = st.selectbox(label='Mothers_qualification', options=encoder_Mothers_qualification.classes_, index=1)
-    data["Mothers_qualification"] = [Mothers_qualification]
+    Fathers_occupation = st.selectbox(label='Fathers_occupation', options=encoder_Fathers_occupation.classes_, index=1)
+    data["Fathers_occupation"] = [Fathers_occupation]
    
 col1, col2 = st.columns(2)
      
