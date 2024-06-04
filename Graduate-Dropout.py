0917719fede7c1752bdeadc847524948b41af9da
Taufik Syah Mauludin <taufiksyah24@gmail.com>
2024-06-04 19:01:48 +0700

    Update Graduate-Dropout.py

--- Graduate-Dropout.py
+++ Graduate-Dropout.py
@@ -75,10 +75,7 @@ with st.expander("View the Raw Data"):
     st.dataframe(data=data, width=800, height=10)
 
 if st.button('Predict'):
-    new_data = data_preprocessing(data=data)
-    st.write((data['Marital_status']))
-    st.write((encoder_Marital_status.classes_))
-    #df["Marital_status"] = encoder_Marital_status.transform(data["Marital_status"])
-    #with st.expander("View the Preprocessed Data"):
-    #    st.dataframe(data=new_data, width=800, height=10)
-    #st.write("Status: {}".format(prediction(new_data)))
+    df["Marital_status"] = encoder_Marital_status.transform(data["Marital_status"])
+    with st.expander("View the Preprocessed Data"):
+        st.dataframe(data=new_data, width=800, height=10)
+    st.write("Status: {}".format(prediction(new_data)))
