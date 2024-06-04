a121785a7a1bdb0aa9c4829ca91eb0a4641e7dc3
Taufik Syah Mauludin <taufiksyah24@gmail.com>
2024-06-04 18:47:54 +0700

    Update data_preprocessing.py

--- data_preprocessing.py
+++ data_preprocessing.py
@@ -29,15 +29,15 @@ def data_preprocessing(data):
     data = data.copy()
     df = pd.DataFrame()
 
-    df["Application_mode"] = encoder_Application_mode.transform(data["Application_mode"])
+    df["Application_mode"] = encoder_Application_mode
     df["Course"] = encoder_Course.transform(data["Course"])
     df["Marital_status"] = encoder_Marital_status
-    df["Nacionality"] = encoder_Nacionality.transform(data["Nacionality"])
-    df["Previous_qualification"] = encoder_Previous_qualification.transform(data["Previous_qualification"])
-    df["Fathers_occupation"] = encoder_Fathers_occupation.transform(data["Fathers_occupation"])
-    df["Fathers_qualification"] = encoder_Fathers_qualification.transform(data["Fathers_qualification"])
-    df["Mothers_occupation"] = encoder_Mothers_occupation.transform(data["Mothers_occupation"])
-    df["Mothers_qualification"] = encoder_Mothers_qualification.transform(data["Mothers_qualification"])
+    df["Nacionality"] = encoder_Nacionality
+    df["Previous_qualification"] = encoder_Previous_qualification
+    df["Fathers_occupation"] = encoder_Fathers_occupation
+    df["Fathers_qualification"] = encoder_Fathers_qualification
+    df["Mothers_occupation"] = encoder_Mothers_occupation
+    df["Mothers_qualification"] = encoder_Mothers_qualification
     df["Curricular_units_1st_sem_approved"] = scaler_Curricular_units_1st_sem_approved.transform(np.asarray(data["Curricular_units_1st_sem_approved"]).reshape(-1,1))[0]
     df["Curricular_units_1st_sem_grade"] = scaler_Curricular_units_1st_sem_grade.transform(np.asarray(data["Curricular_units_1st_sem_grade"]).reshape(-1,1))[0]
     df["Curricular_units_2nd_sem_approved"] = scaler_Curricular_units_2nd_sem_approved.transform(np.asarray(data["Curricular_units_2nd_sem_approved"]).reshape(-1,1))[0]
