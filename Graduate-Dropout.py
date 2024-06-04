cd45b64f4ff4a5300ed3e97d541d999a3243f62a
Taufik Syah Mauludin <taufiksyah24@gmail.com>
2024-06-04 19:18:19 +0700

    Update Graduate-Dropout.py

--- Graduate-Dropout.py
+++ Graduate-Dropout.py
@@ -75,6 +75,7 @@ with st.expander("View the Raw Data"):
     st.dataframe(data=data, width=800, height=10)
 
 if st.button('Predict'):
+    new_data = data_preprocessing(data=data)
     with st.expander("View the Preprocessed Data"):
         st.dataframe(data=new_data, width=800, height=10)
     st.write("Status: {}".format(prediction(new_data)))
